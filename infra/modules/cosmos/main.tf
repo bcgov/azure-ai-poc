efce84cc59e98415730aa29e9d8496ab67f61309
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-08-02 10:56:54 -0700

    remove PE from infra

--- infra/modules/cosmos/main.tf
+++ infra/modules/cosmos/main.tf
@@ -53,22 +53,6 @@ resource "azurerm_monitor_diagnostic_setting" "cosmosdb_sql_diagnostics" {
 
 }
 
-resource "azurerm_private_endpoint" "cosmosdb_sql_db_private_endpoint" {
-  name                = "${var.app_name}-cosmosdb-pe"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.private_endpoint_subnet_id
-  private_service_connection {
-    name                           = "${azurerm_cosmosdb_account.cosmosdb_sql.name}_privateserviceconnection"
-    private_connection_resource_id = azurerm_cosmosdb_account.cosmosdb_sql.id
-    is_manual_connection           = false
-    subresource_names              = ["sql"]
-  }
-  tags = var.common_tags
-  lifecycle {
-    ignore_changes = [tags, private_dns_zone_group]
-  }
-}
 
 resource "azurerm_cosmosdb_sql_database" "cosmosdb_sql_db" {
   name                = var.cosmosdb_sql_database_name
