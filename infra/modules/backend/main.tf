1059dc906f55815db23a2542e33284107bc26b14
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-08-03 14:43:25 -0700

    remove autoscaler

--- infra/modules/backend/main.tf
+++ infra/modules/backend/main.tf
@@ -97,58 +97,6 @@ resource "azurerm_linux_web_app" "backend" {
   }
 }
 
-# Backend Autoscaler
-resource "azurerm_monitor_autoscale_setting" "backend_autoscale" {
-  name                = "${var.app_name}-backend-autoscale"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  target_resource_id  = azurerm_service_plan.backend.id
-  enabled             = var.backend_autoscale_enabled
-  profile {
-    name = "default"
-    capacity {
-      default = 2
-      minimum = 1
-      maximum = 10
-    }
-    rule {
-      metric_trigger {
-        metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.backend.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 70
-      }
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-    rule {
-      metric_trigger {
-        metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.backend.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 30
-      }
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-  }
-}
 # Backend Diagnostics
 resource "azurerm_monitor_diagnostic_setting" "backend_diagnostics" {
   name                       = "${var.app_name}-backend-diagnostics"
